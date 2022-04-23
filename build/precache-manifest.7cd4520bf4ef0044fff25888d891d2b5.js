62b7163137a559dd1e3a68f313f32efb41da17bd
carocollazo <67442372+carocollazo@users.noreply.github.com>
2022-04-22 23:50:09 -0500

    final

---
+++ build/precache-manifest.7cd4520bf4ef0044fff25888d891d2b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71bdaf5e4818327127b1",
+    "url": "/uiux-development/static/css/main.66c6c061.chunk.css"
+  },
+  {
+    "revision": "71bdaf5e4818327127b1",
+    "url": "/uiux-development/static/js/main.71bdaf5e.chunk.js"
+  },
+  {
+    "revision": "8c98aab82769540efb62",
+    "url": "/uiux-development/static/js/1.8c98aab8.chunk.js"
+  },
+  {
+    "revision": "45441cab367a905c86f9",
+    "url": "/uiux-development/static/js/2.45441cab.chunk.js"
+  },
+  {
+    "revision": "b7b8c6a0a113691aa8e0",
+    "url": "/uiux-development/static/js/runtime~main.b7b8c6a0.js"
+  },
+  {
+    "revision": "14a988943885d5299c6aee255061faab",
+    "url": "/uiux-development/index.html"
+  }
+];
